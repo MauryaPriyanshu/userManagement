4f2c0bfc1440fd57ebf9c306a671f3fdc22cd0cf
MauryaPriyanshu <priyanshu9517@gmail.com>
2023-08-12 21:59:38 +0530

    Initial commit

---
+++ src/main/java/com/gl/userManagementApp/expection/MandataoryParamNotPresentException.java
@@ -0,0 +1,9 @@
+package com.gl.userManagementApp.expection;
+
+public class MandataoryParamNotPresentException extends RuntimeException{
+
+    public MandataoryParamNotPresentException(String message){
+
+        super(message);
+    }
+}
---
+++ src/main/java/com/gl/userManagementApp/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.gl.userManagementApp.repository;
+import com.gl.userManagementApp.dto.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+    public interface UserRepository extends CrudRepository<User,Long>{
+
+}
